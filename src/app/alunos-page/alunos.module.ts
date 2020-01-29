3dafbc3fbd32d71bda93dda20bc0d3cc4d26e4da
Maxsuel Storch <maxsuelstorch@gmail.com>
2020-01-29 13:07:38 -0300

    Corrigido alunos

--- src/app/alunos-page/alunos.module.ts
+++ src/app/alunos-page/alunos.module.ts
@@ -1,25 +1,22 @@
-import { AlunosService } from './alunos.service';
 import { RouterModule } from '@angular/router';
-import { AlunosPageComponent } from './alunos-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AlunosCadastrarComponent } from './alunos-cadastrar/alunos-cadastrar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { SharedModule } from '../shared/shared.module';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputTextModule } from 'primeng/inputtext';
-import { TooltipModule } from 'primeng/tooltip';
+
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CheckboxModule } from 'primeng/checkbox';
-import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { AlunosService } from './alunos.service';
+import { AlunosPageComponent } from './alunos-page.component';
+import { AlunosCadastrarComponent } from './alunos-cadastrar/alunos-cadastrar.component';
+import { SharedModule } from '../shared/shared.module';
 import { AlunosListagemComponent } from './alunos-listagem/alunos-listagem.component';
-import {TableModule} from 'primeng/table';
-import { HttpClientModule } from '@angular/common/http';
-import {MultiSelectModule} from 'primeng/multiselect';
 import { AlunosDetalharComponent } from './alunos-detalhar/alunos-detalhar.component';
-import {ListboxModule} from 'primeng/listbox';
 
 @NgModule({
   declarations: [
@@ -32,24 +29,20 @@ import {ListboxModule} from 'primeng/listbox';
     CommonModule,
     FormsModule,
     SharedModule,
-    InputMaskModule,
     InputTextModule,
     ButtonModule,
-    TooltipModule,
     CalendarModule,
     BrowserAnimationsModule,
-    CheckboxModule,
-    DropdownModule,
     RouterModule,
     TableModule,
     HttpClientModule,
-    MultiSelectModule,
-    ListboxModule
+    MultiSelectModule
   ],
   exports: [
     AlunosCadastrarComponent,
     AlunosPageComponent,
-    AlunosListagemComponent
+    AlunosListagemComponent,
+    AlunosDetalharComponent
   ],
   providers: [
     AlunosService
